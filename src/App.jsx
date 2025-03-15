9306cd471676fcfa3cbae057ca21a38476f42c18
Vchuks <64593598+Vchuks@users.noreply.github.com>
2025-03-15 09:31:45 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 function App() {
 
