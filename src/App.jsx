68ee2e12a0daba4e11c89776de1e79d2ac65ee80
Vchuks <64593598+Vchuks@users.noreply.github.com>
2025-03-15 08:48:41 +0100

    UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 
@@ -5,7 +6,11 @@ function App() {
 
   return (
     <>
+    <Routes>
+      <Route path="/" element ={
       <Home/>
+      }/>
+    </Routes>
     </>
   )
 }
